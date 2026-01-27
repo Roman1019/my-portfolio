4d10dfae781a636de7ce1fcb3234e0c9c1201929
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2026-01-27 15:07:10 +0200

    about-me

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -1,30 +1,74 @@
-// import css from "./AboutMe.module.css";
+import css from "./AboutMe.module.css";
 import { FaLinkedin, FaGithub, FaFacebook } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
 import { BsTelephone } from "react-icons/bs";
 export default function AboutMe() {
+  // const developer = {
+  //   name: "fff",
+  //   skills: ["hh"],
+  // };
   return (
     <section>
-      <div>
-        <p>
-          Hi, I`m - Roman Romaniv Full Stack Developer who likes to create new
-          websites.
-        </p>
-        <a href="">
-          <FaLinkedin />
-        </a>
-        <a href="">
-          <FaGithub />
-        </a>
-        <a href="">
-          <FaFacebook />
-        </a>
-        <a href="">
-          <MdAlternateEmail />
-        </a>
-        <a href="">
-          <BsTelephone />
-        </a>
+      <div className={css.container}>
+        <div className={css.headandicons}>
+          <h1>
+            Hi, I`m - Roman Romaniv Full Stack Developer who likes to create new
+            websites.
+          </h1>
+          <ul className={css.iconlist}>
+            <li>
+              <a className={css.iconitem} href="">
+                <FaLinkedin size={18} />
+              </a>
+            </li>
+            <li>
+              <a className={css.iconitem} href="">
+                <FaGithub />
+              </a>
+            </li>
+            <li>
+              <a className={css.iconitem} href="">
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a className={css.iconitem} href="">
+                <MdAlternateEmail />
+              </a>
+            </li>
+            <li>
+              <a className={css.iconitem} href="">
+                <BsTelephone />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <span>const</span>
+            <span>developer</span>
+            <span>=</span>
+            <span>{"{"}</span>
+          </div>
+          <div>
+            <span>name</span>
+            <span>:</span>
+            <span>'Roman Romaniv',</span>
+          </div>
+          <div>
+            <span>skills:</span>
+            <span>{"["}</span>
+            <span>'HTML'</span>
+            <span>,</span>
+            <span>'CSS'</span>
+            <span>,</span>
+            <span>'JavaScript'</span>
+            <span>,</span>
+            <span>'React'</span>
+            <span>,</span>
+            <span>'Node.js'</span>
+          </div>
+        </div>
       </div>
     </section>
   );
