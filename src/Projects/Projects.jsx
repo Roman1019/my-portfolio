aa4a0b148cd13a7ffbe594466ff9bea30c690cd1
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2026-01-29 15:13:40 +0200

    about-me code

---
+++ src/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import css from "./Projects.module.css";
+
+export default function Projects() {
+  return (
+    <section>
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </section>
+  );
+}
